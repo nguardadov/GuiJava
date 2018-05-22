5683922fee74d8df0bc931216ace2d3031a75617
JorgeAzzuFranco <00074216@uca.edu.sv>
2018-05-22 08:46:54 -0600

    Hacer clase Main

---
+++ src/labo4poo/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labo4poo;
+
+import gui.Ventana;
+
+/**
+ *
+ * @author uca
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Ventana().setVisible(true);
+        
+        
+        String hola="hola mundo";
+       //System.out.println(hola.);
+    }
+}
